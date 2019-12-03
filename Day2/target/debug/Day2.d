1946c80252ea4ad6c2dadd79987b4cc7458825b6
Cory Lamontagne <corylamontagne@yahoo.com>
2019-12-02 22:56:43 -0500

    remove targets

--- Day2/target/debug/Day2.d
+++
@@ -1,1 +0,0 @@
-D:\dev\AOC\Day2\target\debug\Day2.exe: D:\dev\AOC\Day2\src\main.rs
--- Day2/target/debug/deps/Day2-32939e5073a18d7f.d
+++
@@ -1,5 +0,0 @@
-D:\dev\AOC\Day2\target\debug\deps\Day2-32939e5073a18d7f.exe: src\main.rs
-
-D:\dev\AOC\Day2\target\debug\deps\Day2-32939e5073a18d7f.d: src\main.rs
-
-src\main.rs:
