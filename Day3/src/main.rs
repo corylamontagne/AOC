591492cc451996cd6883e5f9f507b296b492cc58
corylamontagne <corylamontagne@yahoo.com>
2019-12-03 11:43:07 -0500

    Split our instruction tuples

--- Day3/src/main.rs
+++ Day3/src/main.rs
@@ -19,6 +19,16 @@ fn parse_file(file: String) -> Option<String> {
     Some(content)
 }
 
+fn build_wire_instructions(vec: &Vec<&str>) -> Vec<(char, i32)> {
+    let mut results = Vec::new();
+    for x in vec {
+        let s = x.chars().next();
+        let ins = x.chars().next().map(|c| &x[c.len_utf8()..]);
+        results.push((s.unwrap(), ins.unwrap().trim_end().parse::<i32>().unwrap()));
+    }
+    return results;
+}
+
 fn main() {
     let data = match parse_file("input.txt".chars().collect()) {
         Some(x) => x,
@@ -28,8 +38,10 @@ fn main() {
     let vec = data.split('\n').collect::<Vec<&str>>();
 
     //get the vector of values for each wire
-    let _wire1 = vec[0].split('\n').collect::<Vec<&str>>();
-    let _wire2 = vec[1].split('\n').collect::<Vec<&str>>();
+    let _wire1 = vec[0].split(',').collect::<Vec<&str>>();
+    let _wire2 =& vec[1].split(',').collect::<Vec<&str>>();
 
-    
+    let _wire1_instr = build_wire_instructions(&_wire1);
+    let _wire2_instr = build_wire_instructions(&_wire2);
+
 }
